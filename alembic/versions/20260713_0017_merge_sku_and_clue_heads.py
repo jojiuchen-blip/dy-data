e085885b5cd628ead99b2dce27209011834478e8
jojiuchen-blip <jojiuchen@gmail.com>
2026-07-13 16:12:06 +0800

    fix: merge alembic migration heads

---
+++ alembic/versions/20260713_0017_merge_sku_and_clue_heads.py
@@ -0,0 +1,22 @@
+"""merge SKU product scope and clue allocation migration heads
+
+Revision ID: 20260713_0017
+Revises: 20260707_0012, 20260712_0016
+Create Date: 2026-07-13 16:10:00
+"""
+
+from __future__ import annotations
+
+
+revision = "20260713_0017"
+down_revision = ("20260707_0012", "20260712_0016")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
