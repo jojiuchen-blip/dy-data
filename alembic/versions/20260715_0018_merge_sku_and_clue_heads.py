fe5bbffb99957671cdb29d1caf4377d30f1f5473
jojiuchen-blip <jojiuchen@gmail.com>
2026-07-15 14:17:35 +0800

    fix: resolve alembic revision collision after main merge

--- alembic/versions/20260715_0018_merge_sku_and_clue_heads.py
+++ alembic/versions/20260715_0018_merge_sku_and_clue_heads.py
@@ -1,15 +1,15 @@
 """merge SKU product scope and clue allocation migration heads
 
-Revision ID: 20260713_0017
-Revises: 20260707_0012, 20260712_0016
-Create Date: 2026-07-13 16:10:00
+Revision ID: 20260715_0018
+Revises: 20260707_0012, 20260713_0017
+Create Date: 2026-07-15 14:15:00
 """
 
 from __future__ import annotations
 
 
-revision = "20260713_0017"
-down_revision = ("20260707_0012", "20260712_0016")
+revision = "20260715_0018"
+down_revision = ("20260707_0012", "20260713_0017")
 branch_labels = None
 depends_on = None
 
